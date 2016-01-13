401a2be47ad014d7fd849afa806f0535b3b19083
aurum <aurum.gallente@gmail.com>
2016-01-13 22:55:11 +0300

    working with callback hell

--- index.js
+++ index.js
@@ -3,31 +3,31 @@ var app = express();
 var fs = require('fs');
 var eolPosition;
 var result='';
-var char;
+var myChar='';
+var fileName = './error-dev.log';
+var buffer;
+var res;
+function setChar(nextChar, cb){
+    res = myChar.concat(nextChar);
+    myChar += nextChar;    
+    (!!cb) ? cb(myChar) : false;
+    return myChar;
+}
+function getFuckingString(cb){
+    fs.open(fileName, 'r', function(status, fd) {
+        if (status) {
+            console.log(status.message);
+            return;
+        }
+        buffer = new Buffer(100);
+        buffer.fill(0);
+        fs.read(fd, buffer, 0, 4, 0, function(err, num) {
+            setChar(buffer.toString(), cb);
+    });
+}
 app.get('/', function(req, res) {
     var start = 0;
-		var a = '';
-    var fileName = './error-dev.log';
-fs.open(fileName, 'r', function(status, fd) {
-    if (status) {
-        console.log(status.message);
-        return;
-    }
-
-		do {
-			var buffer = new Buffer(1);
-			buffer.fill(0);
-			var a=fs.readSync(fd, buffer, 0, 1, start, function(err, num, buff) {
-					char = buff.toString();
-					result += char;
-					//console.log(start);
-					//console.log(result.indexOf("\n"));
-					return char;
-			});
-			console.log(char);
-			start++;
-		}
-		while(start<160);
-	});
+    res='';
+    getFuckingString(console.log);
 });
 app.listen(3000);
